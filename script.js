e80ac1731dbd4c9d50678f941a47883cb703b25c
rodrigo <24rodrigocurso@gmail.com>
2025-02-04 20:03:36 -0300

    Barra de pesquisa ainda não terminada

---
+++ script.js
@@ -0,0 +1,8 @@
+const content = document.querySelector(".content");
+const inputSearch = document.querySelector("input[type='search']");
+
+let items = [];
+//aprender a usar o fetch para continuar o tutorial
+const URL ='https://dummyjson.com/products';
+
+fetch(URL).then()
