da388be2eb92e18947302da7505e5bf633b7e41b
rodrigo <24rodrigocurso@gmail.com>
2025-02-05 06:16:44 -0300

    barra de pesquisa pronta

--- script.js
+++ script.js
@@ -3,6 +3,27 @@ const inputSearch = document.querySelector("input[type='search']");
 
 let items = [];
 //aprender a usar o fetch para continuar o tutorial
-const URL ='https://dummyjson.com/products';
+const URL ='https://jsonplaceholder.typicode.com/users';
 
-fetch(URL).then()
+inputSearch.oninput =()=>{
+    content.innerHTML ="";
+    items
+        .filter((item)=> 
+        item.toLowerCase().includes(inputSearch.value.toLocaleLowerCase())
+    )
+        .forEach((item)=> addHTML(item));
+    
+};
+function addHTML(item){
+    const div = document.createElement("div");
+    div.innerHTML = item;
+    content.append(div);
+}
+fetch(URL).then(
+    (data)=> data.json()
+).then((users) => {
+    users.forEach((user) => {
+        addHTML(user.name);
+        items.push(user.name);
+    });
+});
